f92bceaaa264f3a77932850da964cca68c11a1b5
Duck1712 <matej.25medakovic@gmail.com>
2023-11-27 12:19:57 +0100

    Made Home.tsx and Navbar.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Home = () => 
+{
+    return(
+        <PageContainer>
+        </PageContainer>
+    )
+}
+
+export const PageContainer = styled.div`
+    background-color:#D7FFFB;
+    width:100%;
+    height:600px;
+`
