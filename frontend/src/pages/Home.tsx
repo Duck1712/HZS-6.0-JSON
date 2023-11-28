72db6cea9c0bee0186b9756bc6ca58a8666d5d43
Duck1712 <matej.25medakovic@gmail.com>
2023-11-28 10:43:44 +0100

    Added TextContainer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components"
+import { TextContainer } from "../sharedComponents/TextContainer"
 
 export const Home = () => 
 {
     return(
         <PageContainer>
+            <TextContainer>
+                <h1>Test!</h1>
+            </TextContainer>
         </PageContainer>
     )
 }
@@ -12,4 +16,5 @@ export const PageContainer = styled.div`
     background-color:#D7FFFB;
     width:100%;
     height:600px;
+    padding:20px;
 `
