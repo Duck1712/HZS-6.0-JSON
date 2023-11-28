7dde8d7406387ad9221f16892c7c7b3b7babe6c3
Duck1712 <matej.25medakovic@gmail.com>
2023-11-28 12:06:08 +0100

    Built database

--- Backend/DataLayer/DbContext.cs
+++ Backend/DataLayer/DbContext.cs
@@ -4,6 +4,8 @@ using HZS.Models;
 public class Context:DbContext
 {
     public DbSet<User> users { get; set; }
+    public DbSet<Post> posts { get; set; }
+    
     public string DbPath { get; }
 
     public Context()
