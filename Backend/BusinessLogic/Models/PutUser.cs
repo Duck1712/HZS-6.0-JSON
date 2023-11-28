29b228bcdbbf4a8779913c687dd48d9bf9e1ac7b
Nikola <nikolageneralovic@skiff.com>
2023-11-28 10:05:18 +0100

    Updating PuTUser and AddUser.

--- Backend/BusinessLogic/Models/PutUser.cs
+++ Backend/BusinessLogic/Models/PutUser.cs
@@ -4,5 +4,8 @@ namespace HZS.Models{
     public class PutUser{
         [JsonIgnoreAttribute]
         public Guid Id { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
     }
 }
